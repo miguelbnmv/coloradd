931100a37330a49de80e5a81171c3347c7c3f96a
dbgfribeiro <dribeiro641@gmail.com>
2019-10-24 12:31:29 +0100

    Fixed header

--- js/form.js
+++ js/form.js
@@ -1,25 +1,16 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+var form = document.getElementById("hiddenForm");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+function revealForm() {
+  form.style.display = "block"
+}
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  form.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == form) {
+    form.style.display = "none";
   }
 }
